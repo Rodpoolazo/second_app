39b618e7f71fd287f1d7865bebe9ff6aee09c226
Rodpoolazo <rodpoolazo@gmail.com>
2019-05-29 21:24:59 -0400

    add email

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_234520) do
+ActiveRecord::Schema.define(version: 2019_05_30_011019) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
